a24013cf4038dfde7a1cbdcbe065bc480f2ba79c
sanjay.joister <snajay.sinalkar@joister.net>
2019-04-26 11:34:10 +0000

    Generated code by SwaggerHub

---
+++ python-flask-api/swagger_server/controllers/customer_controller.py
@@ -0,0 +1,86 @@
+import connexion
+import six
+
+from swagger_server.models.api_response import ApiResponse  # noqa: E501
+from swagger_server.models.customer import Customer  # noqa: E501
+from swagger_server import util
+
+
+def create_customer(body):  # noqa: E501
+    """Create new customer
+
+     # noqa: E501
+
+    :param body: Customer object that needs to be added to the customer table
+    :type body: dict | bytes
+
+    :rtype: ApiResponse
+    """
+    if connexion.request.is_json:
+        body = Customer.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def customer_login():  # noqa: E501
+    """Customer Login to System
+
+     # noqa: E501
+
+
+    :rtype: str
+    """
+    return 'do some magic!'
+
+
+def customer_logout():  # noqa: E501
+    """Logs out current logged in customer session
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def delete_user(vUsername):  # noqa: E501
+    """Delete user
+
+    This can only be done by the logged in user. # noqa: E501
+
+    :param vUsername: The name that needs to be deleted
+    :type vUsername: str
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def get_customer_by_user_name(vUsername):  # noqa: E501
+    """Get user by user name
+
+     # noqa: E501
+
+    :param vUsername: The name that needs to be fetched. Use user1 for testing.
+    :type vUsername: str
+
+    :rtype: Customer
+    """
+    return 'do some magic!'
+
+
+def update_customer(vUsername, body):  # noqa: E501
+    """Updated user
+
+    This can only be done by the logged in user. # noqa: E501
+
+    :param vUsername: name that need to be updated
+    :type vUsername: str
+    :param body: Updated customer object
+    :type body: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        body = Customer.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
---
+++ python-flask-api/swagger_server/models/__init__.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.api_response import ApiResponse
+from swagger_server.models.customer import Customer
+from swagger_server.models.customer_billing_address import CustomerBillingAddress
+from swagger_server.models.customer_category import CustomerCategory
+from swagger_server.models.customer_shipping_address import CustomerShippingAddress
+from swagger_server.models.invoice import Invoice
+from swagger_server.models.invoice_billing_address import InvoiceBillingAddress
+from swagger_server.models.invoice_brand import InvoiceBrand
+from swagger_server.models.invoice_item_billing_cycle import InvoiceItemBillingCycle
+from swagger_server.models.invoice_product import InvoiceProduct
+from swagger_server.models.invoice_product_item import InvoiceProductItem
+from swagger_server.models.invoice_shipping_address import InvoiceShippingAddress
+from swagger_server.models.invoice_tax import InvoiceTax
