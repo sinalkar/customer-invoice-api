7dbfe54ddebe8a3835fe307d5322a1ce949c3586
sanjay.joister <sanjay.sinalkar@joister.net>
2019-05-03 07:14:26 +0000

    Generated code by SwaggerHub

--- python-flask-api/swagger_server/controllers/customer_controller.py
+++ python-flask-api/swagger_server/controllers/customer_controller.py
@@ -3,6 +3,8 @@ import six
 
 from swagger_server.models.api_response import ApiResponse  # noqa: E501
 from swagger_server.models.customer import Customer  # noqa: E501
+from swagger_server.models.invoice import Invoice  # noqa: E501
+from swagger_server.models.invoice_product import InvoiceProduct  # noqa: E501
 from swagger_server import util
 
 
@@ -30,6 +32,62 @@ def get_customer_by_id(iCustomerId):  # noqa: E501
     :type iCustomerId: str
 
     :rtype: Customer
+    """
+    return 'do some magic!'
+
+
+def get_customer_invoice_by_id(iCustomerId, iInvoiceId):  # noqa: E501
+    """Get Customer invoice detail of given customer id
+
+    Logged In customer get own invoice list # noqa: E501
+
+    :param iCustomerId: Customer Id of logged In user
+    :type iCustomerId: str
+    :param iInvoiceId: Invoice id to be fetch from 
+    :type iInvoiceId: str
+
+    :rtype: object
+    """
+    return 'do some magic!'
+
+
+def get_customer_invoice_list(iCustomerId):  # noqa: E501
+    """Get Customer invoice list of given customer id
+
+    Logged In customer get own invoice list # noqa: E501
+
+    :param iCustomerId: Customer Id needed to fetch all profile related information of given customer
+    :type iCustomerId: str
+
+    :rtype: List[Invoice]
+    """
+    return 'do some magic!'
+
+
+def get_customer_product_detail_by_product_id(iProductId, iCustomerId):  # noqa: E501
+    """Get product detail for logged in customer
+
+    Logged In customer get product details by ProductId of related regional  # noqa: E501
+
+    :param iProductId: iProductId Id of logged In user
+    :type iProductId: str
+    :param iCustomerId: iProductId Id of logged In user
+    :type iCustomerId: str
+
+    :rtype: object
+    """
+    return 'do some magic!'
+
+
+def get_customers_product_list(iCustomerId):  # noqa: E501
+    """Get product list for logged in customer
+
+    Logged In customer get product list of related region # noqa: E501
+
+    :param iCustomerId: Customer Id of logged In user
+    :type iCustomerId: str
+
+    :rtype: List[InvoiceProduct]
     """
     return 'do some magic!'
 
--- python-flask-api/swagger_server/test/test_customer_controller.py
+++ python-flask-api/swagger_server/test/test_customer_controller.py
@@ -7,6 +7,8 @@ from six import BytesIO
 
 from swagger_server.models.api_response import ApiResponse  # noqa: E501
 from swagger_server.models.customer import Customer  # noqa: E501
+from swagger_server.models.invoice import Invoice  # noqa: E501
+from swagger_server.models.invoice_product import InvoiceProduct  # noqa: E501
 from swagger_server.test import BaseTestCase
 
 
@@ -34,6 +36,50 @@ class TestCustomerController(BaseTestCase):
         """
         response = self.client.open(
             '/joinet/customer/1.0.0/customer/{iCustomerId}/profile'.format(iCustomerId='iCustomerId_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_customer_invoice_by_id(self):
+        """Test case for get_customer_invoice_by_id
+
+        Get Customer invoice detail of given customer id
+        """
+        response = self.client.open(
+            '/joinet/customer/1.0.0/customer/{iCustomerId}/invoice/{iInvoiceId}'.format(iCustomerId='iCustomerId_example', iInvoiceId='iInvoiceId_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_customer_invoice_list(self):
+        """Test case for get_customer_invoice_list
+
+        Get Customer invoice list of given customer id
+        """
+        response = self.client.open(
+            '/joinet/customer/1.0.0/customer/{iCustomerId}/invoice'.format(iCustomerId='iCustomerId_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_customer_product_detail_by_product_id(self):
+        """Test case for get_customer_product_detail_by_product_id
+
+        Get product detail for logged in customer
+        """
+        response = self.client.open(
+            '/joinet/customer/1.0.0/customer/{iCustomerId}/product/{iProductId}'.format(iProductId='iProductId_example', iCustomerId='iCustomerId_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_customers_product_list(self):
+        """Test case for get_customers_product_list
+
+        Get product list for logged in customer
+        """
+        response = self.client.open(
+            '/joinet/customer/1.0.0/customer/{iCustomerId}/product'.format(iCustomerId='iCustomerId_example'),
             method='GET')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
